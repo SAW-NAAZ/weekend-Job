324c1709291f61fcc176763f74482cb49a1b160a
Shaik Abdul Vahid <59871200+SAW-NAAZ@users.noreply.github.com>
2024-03-17 12:13:37 +0530

    Update hello.sh
    Hello team there was an error i fixed it and updated, kindly check.

--- hello.sh
+++ hello.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 echo "Hello and Welcome to the session"
 echo "Good morning iam from 2nd shift owner of the file"
-
+echo "Good morning iam from 2nd shift owner of the file"
+echo "Good morning iam from 2nd shift owner of the file"
+echo "Good morning iam from 2nd shift owner of the file"
+echo "Good morning iam from 2nd shift owner of the file"
+echo "Good morning iam from 2nd shift owner of the file"
+echo "Good morning iam from 2nd shift owner of the file"
